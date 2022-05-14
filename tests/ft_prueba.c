b4f99a3e5c6c0baefff096cf69eaf638b8fa236b
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-14 14:54:51 +0200

    split code into functions

--- tests/ft_prueba.c
+++ tests/ft_prueba.c
@@ -1,82 +1,68 @@
 #include<stdio.h>
 #include<unistd.h>
 
-void ft_prueba(x,y){
-	
-	//la y son las filas y la x las columnas
-	
-	int i;
-	i=1;
-	int j;
-	j=1;
-
-	while(i<=y){
-		j=1;
-		if(i == 1){
-		
-			while(j<=x){
-
-				if(j == 1){					
-					write(1, "A",1);
-				}
-
-				else if(j == x){				
-					write(1, "B",1);
-				}
-
-				else{
-					write(1,"H", 1);
-				}
-
-				j++;
-			}
-		}
-
-
-		if (i == y){
-		
-			while(j<=x){
-
-				if (j == 1){					
-					write(1, "B",1);
-				}
-
-				else if (j == x){				
-					write(1, "B",1);
-				}
-
-				else{
-					write(1,"H", 1);
-				}
-				j++;
-			}		
-		}
-
-
-		else{
-		
-			while(j<=x){
-
-				if (j == 1){					
-					write(1, "H",1);
-				}
-
-				else if (j == x){				
-					write(1, "H",1);
-				}
-
-				else{
-					write(1," ", 1);
-				}
-
-				j++;
-			}
-			write(1,"\n",1);
-		
-		}
-
-		i++;
-	
+int	side_logic(int j, int x)
+{
+	while (j <= x)
+	{
+		if (j == 1)
+			write(1, "H", 1);
+		else if (j == x)
+			write(1, "H", 1);
+		else
+			write(1, " ", 1);
+		++j;
 	}
+	write(1, "\n", 1);
+	return (j);
+}
 
+int	bottom_logic(int j, int x)
+{
+	while (j <= x)
+	{
+		if (j == 1)
+			write(1, "B", 1);
+		else if (j == x)
+			write(1, "A", 1);
+		else
+			write(1, "H", 1);
+		j++;
+	}
+	return (j);
+}
+
+int	top_logic(int j, int x)
+{
+	while (j <= x)
+	{
+		if (j == 1)
+			write(1, "A", 1);
+		else if (j == x)
+			write(1, "B", 1);
+		else
+			write(1, "H", 1);
+		++j;
+	}
+	return (j);
+}
+
+void	ft_prueba(int x, int y)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 1;
+	while (i <= y)
+	{
+		j = 1;
+		if (i == 1)
+			j = top_logic(j, x);
+		if (i == y)
+			j = bottom_logic(j, x);
+		else
+			j = side_logic(j, x);
+		i++;
+	}
 }
