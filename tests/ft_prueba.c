190b5000bcd432596f1359f7bd810670bf58c862
Yoav Rabby <yrabby@c2r7s3.42madrid.com>
2022-05-14 15:06:05 +0200

    created a generic function

--- tests/ft_prueba.c
+++ tests/ft_prueba.c
@@ -1,50 +1,50 @@
-#include<stdio.h>
 #include<unistd.h>
 
-int	side_logic(int j, int x)
+int	generic_print(int j, int x, char chars_to_print[3])
 {
 	while (j <= x)
 	{
 		if (j == 1)
-			write(1, "H", 1);
+			write(1, &chars_to_print[0], 1);
 		else if (j == x)
-			write(1, "H", 1);
+			write(1, &chars_to_print[1], 1);
 		else
-			write(1, " ", 1);
+			write(1, &chars_to_print[2], 1);
 		++j;
 	}
+	return (j);
+}
+
+int	side_logic(int j, int x)
+{
+	char	chars_to_print[3];
+
+	chars_to_print[0] = 'H';
+	chars_to_print[1] = 'H';
+	chars_to_print[2] = ' ';
+	j = generic_print(j, x, chars_to_print);
 	write(1, "\n", 1);
 	return (j);
 }
 
 int	bottom_logic(int j, int x)
 {
-	while (j <= x)
-	{
-		if (j == 1)
-			write(1, "B", 1);
-		else if (j == x)
-			write(1, "A", 1);
-		else
-			write(1, "H", 1);
-		j++;
-	}
-	return (j);
+	char	chars_to_print[3];
+
+	chars_to_print[0] = 'B';
+	chars_to_print[1] = 'A';
+	chars_to_print[2] = 'H';
+	return (generic_print(j, x, chars_to_print));
 }
 
 int	top_logic(int j, int x)
 {
-	while (j <= x)
-	{
-		if (j == 1)
-			write(1, "A", 1);
-		else if (j == x)
-			write(1, "B", 1);
-		else
-			write(1, "H", 1);
-		++j;
-	}
-	return (j);
+	char	chars_to_print[3];
+
+	chars_to_print[0] = 'A';
+	chars_to_print[1] = 'B';
+	chars_to_print[2] = 'H';
+	return (generic_print(j, x, chars_to_print));
 }
 
 void	ft_prueba(int x, int y)
